ca8d08614aa43c8babf1183159d6c75d0af78fe1
Charles Milette <charles.milette@gmail.com>
2021-06-22 05:40:15 -0400

    Update image and copyright notices

--- transform_binary_payload_pilot_things/src-iotrule-transformation/app.py
+++ transform_binary_payload_pilot_things/src-iotrule-transformation/app.py
@@ -1,4 +1,4 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright Pilot Things. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this
